e97e40156b50ca3207fcedcd9d294e1a4b93bdb0
Filip C <bengtfille2@gmail.com>
2026-05-28 15:37:21 +0200

    accordion

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,30 +1,44 @@
-import type { ReactNode } from "react";
+import './Accordion.css'
+import { useState, Children, cloneElement, isValidElement, type ReactNode } from "react";
 
 interface AccordionItemType {
   title: string;
   content: string;
+  index?: number; 
 }
 
 interface AccordionProps {
   children: ReactNode;
 }
 
-export const AccordionItem = ({
-  title,
-  content,
-}: AccordionItemType) => {
+
+export const AccordionItem = ({ title, content, index }: AccordionItemType) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <div>{title}</div>
-      <div>{content}</div>
-    </>
+    <div className="accordion-header">
+      <button onClick={() => setIsOpen(prev => !prev)} className="accordion-btn">
+        <span className='accordion-title'>
+          <span className='accordion-index'>{String(index).padStart(2, '0')}</span> {title}
+        </span>
+        <span className={`accordion-arrow ${isOpen ? 'accordion-arrow--open' : ''}`}>→
+</span>
+      </button>
+      {isOpen && (
+        <div className="accordion-content">{content}</div>
+      )}
+    </div>
   );
 };
 
 export const Accordion = ({ children }: AccordionProps) => {
   return (
     <>
-      {children}
+      {Children.map(children, (child, i) =>
+        isValidElement<AccordionItemType>(child)
+          ? cloneElement(child, { index: i + 1 })
+          : child
+      )}
     </>
   );
 };
